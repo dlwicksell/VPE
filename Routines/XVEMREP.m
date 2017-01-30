3eeb38883b2f894c9fb69af6112689dccf52850e
Sam Habiel <sam.habiel@gmail.com>
2017-01-29 20:46:54 -0800

    ESC-G support to view global

--- Routines/XVEMREP.m
+++ Routines/XVEMREP.m
@@ -1,4 +1,4 @@
-XVEMREP ;DJB,VRR**EDIT - Web,Html,Parse Rtn/Global,RETURN ; 11/6/16 10:24pm
+XVEMREP ;DJB,VRR**EDIT - Web,Html,Parse Rtn/Global,RETURN ; 11/13/16 7:26pm
  ;;13.1;VICTORY PROG ENVIRONMENT;;May 23, 2016
  ;
 WEB ;Web Mode
@@ -38,25 +38,13 @@ PARSE ;Run rtn name from code at cursor position
  . I RTN["%",$E(RTN)'="%" S RTN=""
  . I $E(RTN) S RTN="" ; for a routine, 1st char can't be numeric
  ;
- ;
- ;
- E  D  ; (sam): I think this is inactive code; but will leave it in for now.
- . S TAG=$E(LINE,10,XCUR)
- . I $L(TAG)>8 S TAG=$E(TAG,$L(TAG)-7,$L(TAG))
- . E  D
- . . S TMP=$G(^TMP("XVV","IR"_VRRS,$J,YND-1))
- . . S TAG=$E(TMP,$L(TMP)-(8-$L(TAG)+1),$L(TMP))_TAG
- . S RTN=$E(LINE,XCUR+2,XCUR+9)
- ;
- ;Find starting point of TAG ; (sam): Not needed anymore due to new algorithm.
- ;F I=$L(TAG):-1:1 I $E(TAG,I)'?1AN,$E(TAG,I)'?1"%" D  Q
- ;. S TAG=$E(TAG,(I+1),$L(TAG))
- ;
- ;If rtn name has scrolled to a 2nd line
  I $L(RTN)'>7 D  ;
  . S TMP=$G(^TMP("XVV","IR"_VRRS,$J,(YND+1)))
  . Q:TMP[$C(30)  Q:TMP=" <> <> <>"
  . S RTN=RTN_$E(TMP,10,9+8-$L(RTN))
+ ;
+ ; Check the routine exists
+ I $T(@(TAG_"^"_RTN))="" W $C(7) Q
  ;
  ;Save RTN & TAG before clearing symbol table.
  S ^TMP("XVV",$J)=RTN_"^"_TAG
@@ -84,23 +72,29 @@ GETRTN() ;Parse routine name
  Q RTN
  ;===================================================================
 GLB ;Select global for viewing by hitting <ESCG>
- NEW DIFF,TMP,TMP1,ZX,ZY,ZZ
- I $G(FLAGGLB)']"" D  S KEY="S" Q
- . S TMP=$G(^TMP("XVV","IR"_VRRS,$J,YND))
- . I TMP[$C(30) S TMP=XCUR+2_"^"_$E(TMP,XCUR+2,XCUR+32)
- . E  S TMP=XCUR+1_"^"_$E(TMP,XCUR+1,XCUR+31)
- . S FLAGGLB=$P(TMP,"^",1)_"^"_YND_"^"_$P(TMP,"^",2,999)
- . S TMP=$P(FLAGGLB,"^",3,999)  Q:$L(TMP)>29
+ ;(sam): Notes below.
+ NEW DIFF,TMP,TMP1,ZX,ZY,ZZ,KEY
+ I $G(FLAGGLB)']"" D  S KEY="S"  ; Don't know what KEY="S" means now.
+ . S TMP=$G(^TMP("XVV","IR"_VRRS,$J,YND)) ; Get line
+ . I TMP[$C(30) S TMP=XCUR+2_"^"_$E(TMP,XCUR+2,XCUR+32) ; Get cursor posotion ^ read 30 chars
+ . E  S TMP=XCUR+1_"^"_$E(TMP,XCUR+1,XCUR+31) ; shouldn't run
+ . S FLAGGLB=$P(TMP,"^",1)_"^"_YND_"^"_$P(TMP,"^",2,999) ; FLAGGLB = Cursor pos ^ line number ^ global
+ . S TMP=$P(FLAGGLB,"^",3,999)  Q:$L(TMP)>29  ; Read the next line for continuation
  . S TMP1=$G(^(YND+1))  Q:TMP1[$C(30)  Q:TMP1=" <> <> <>"
  . S FLAGGLB=FLAGGLB_$E(TMP1,10,10+30-$L(TMP))
  ;
+ ; ZX = cur pos, ZY = line number; ZZ global
  S ZX=$P(FLAGGLB,"^",1),ZY=$P(FLAGGLB,"^",2),ZZ=$P(FLAGGLB,"^",3,999)
- S DIFF=$S($G(^TMP("XVV","IR"_VRRS,$J,YND))[$C(30):XCUR+2,1:XCUR+1)
- I (YND-ZY)>1 KILL FLAGGLB Q  ;Moved cursor too far
- I (ZY>YND) KILL FLAGGLB Q  ;Moved cursor up
- I YND>ZY S DIFF=ZX+(XVV("IOM")-ZX)+DIFF-10 I ZX>DIFF KILL FLAGGLB Q
- S DIFF=DIFF-ZX+1,FLAGGLB=$E(ZZ,1,DIFF)
- S ^TMP("XVV",$J)=FLAGGLB KILL FLAGGLB
+ ; (sam): Next 5 lines make no sense to me.
+ ;S DIFF=$S($G(^TMP("XVV","IR"_VRRS,$J,YND))[$C(30):XCUR+2,1:XCUR+1)
+ ;I (YND-ZY)>1 KILL FLAGGLB Q  ;Moved cursor too far
+ ;I (ZY>YND) KILL FLAGGLB Q  ;Moved cursor up
+ ;I YND>ZY S DIFF=ZX+(XVV("IOM")-ZX)+DIFF-10 I ZX>DIFF KILL FLAGGLB Q
+ ;S DIFF=DIFF-ZX+1,FLAGGLB=$E(ZZ,1,DIFF)
+ ;S ^TMP("XVV",$J)=FLAGGLB KILL FLAGGLB
+ S ZZ=$$PARSEGLB(ZZ)
+ I ZZ="" QUIT
+ S ^TMP("XVV",$J)=ZZ KILL FLAGGLB
  D ENDSCR^XVEMKT2
  D SYMTAB^XVEMKST("C","VRR",VRRS) ;Save symbol table
  D PARAM^XVEMG(^TMP("XVV",$J))
@@ -108,6 +102,42 @@ GLB ;Select global for viewing by hitting <ESCG>
  D SYMTAB^XVEMKST("R","VRR",VRRS) ;Restore symbol table
  D REDRAW1^XVEMRU
  Q
+PARSEGLB(G) ; [Internal] Parse the global into a format XVEMG recognizes
+ N FLAGQ S FLAGQ=0
+ N DONE S DONE=0
+ N GOUT S GOUT=""
+ N MODE S MODE="GNAME"
+ N I,C
+ D
+ . I $E(G)'="^" QUIT
+ . S GOUT="^"
+ . F I=2:1:$L(G) D  Q:FLAGQ  Q:DONE  S C=$E(G,I) D @MODE
+ I MODE="GPQUOTE" S GOUT=$P(GOUT,"""") ; Abnormal termination. Get first part.
+ I $E(GOUT,$L(GOUT))="," S $E(GOUT,$L(GOUT))=""
+ I $E(GOUT,$L(GOUT)-1,$L(GOUT))=",0" S $E(GOUT,$L(GOUT)-1,$L(GOUT))=""
+ Q GOUT
+GNAME ; [Internal] Get global name
+ I C?.1"%".A S GOUT=GOUT_C QUIT
+ I C="(" S GOUT=GOUT_C,MODE="GOPAR" QUIT
+ S FLAGQ=1
+ QUIT
+GOPAR ; [Internal] Open parens
+ I C="""" S GOUT=GOUT_C S MODE="GPQUOTE" QUIT
+ I C?1N!(C=".") S GOUT=GOUT_C QUIT  ; Numbers are literals
+ I C="," S GOUT=GOUT_C QUIT  ; Commas are okay
+ I C="$",$E(G,I+1)?1A S GOUT=GOUT_C QUIT  ; ISVs are okay.
+ I C?1A,$E(G,I-1)="$" S GOUT=GOUT_C QUIT  ; Ditto
+ I C=")" S DONE=1,MODE="GCPAR" QUIT  ; Done; don't append
+ S GOUT=GOUT_":,",MODE="GCADV"  ; advance past next comma
+ QUIT
+GCADV ; [Internal] Advance till next comma
+ I C'="," QUIT
+ S MODE="GOPAR"
+ QUIT
+GPQUOTE ; [Internal] Quote inside parens
+ I C'="""" S GOUT=GOUT_C QUIT
+ S MODE="GOPAR",GOUT=GOUT_C
+ QUIT
  ;===================================================================
 RETURN ;Process <RET> key
  ;If new rtn, open new line regardless of parameter setting.
