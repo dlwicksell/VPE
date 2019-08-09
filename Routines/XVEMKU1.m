b1190d58d14e0a1fc88565d9f21b3c667ea8ee9a
David Wicksell <dlw@linux.com>
2019-08-09 16:11:25 -0600

    Update generic error message

--- Routines/XVEMKU1.m
+++ Routines/XVEMKU1.m
@@ -1,4 +1,4 @@
-XVEMKU1 ;DJB/KRN**DATEDASH,CURSOR,YN,TRAP,ERRMSG,DISCLAIM ;2017-08-15  1:17 PM
+XVEMKU1 ;DJB/KRN**DATEDASH,CURSOR,YN,TRAP,ERRMSG,DISCLAIM ;2019-08-09  2:38 PM
  ;;15.1;VICTORY PROG ENVIRONMENT;;Jun 19, 2019
  ; Original Code authored by David J. Bolduc 1985-2005
  ;
@@ -28,7 +28,7 @@ ERRMSG(PKG) ;Generic error message
  S @("ZE="_XVV("$ZE"))
  W $C(7),!!,"NOTE: You've discovered an error in "_PKG_"."
  W !,"Error: ",ZE
- W !,"Please report error to BOLDUC,DAVID@FORUM.VA.GOV.",!
+ W !,"Please create (or update) an issue in the VPE issue tracker at https://github.com/shabiel/VPE/issues.",!
  Q
 DISCLAIM ;Disclaimer
  W !!,"=========================< D I S C L A I M E R >========================="
