dlwicksell/VPE

commit 5779b4a5bd4d91d0c40b9446a5da921d1ac2e20c

Fix namespace issues/symbol table from XINDEX
XVEMKP & XVEMSRL: New %ZIS and ZTSK
XVEMKT{F,S,T,U}, XVEMRID, XVEMRLI, XVEMSQS: Change ^TMP("CE" and
^TMP("VPE" to ^TMP("XVV something in order to start in the XV namespace.
VPE also has the VEE namespace, but eventually I would it like us to
completely move out of there.

Routines/XVEMRLI.m
@@ -1,4 +1,4 @@
-XVEMRLI ;DJB/VRR**RTN LBRY - Sign In Rtns,ALL,Edit IDENTIFIER ;2017-08-15  2:00 PM
+XVEMRLI ;DJB/VRR**RTN LBRY - Sign In Rtns,ALL,Edit IDENTIFIER ;Aug 20, 2019@16:51
  ;;15.1;VICTORY PROG ENVIRONMENT;;Jun 19, 2019
  ; Original Code authored by David J. Bolduc 1985-2005
  ; New Error trap in EN+2,GETRTN (c) 2016 Sam Habiel
@@ -13,20 +13,20 @@ EN ;Use ALL^XVEMRLI if you need to sign in rtns other than your own.
  Q:'$D(^XVV(19200.11))  ;...Library file doesn't exist
  S FLAGQ=0 D INIT^XVEMRLU G:FLAGQ EX
  D GETRTN G:FLAGQ EX
- G:'$D(^TMP("VPE","SELECT",$J)) EX ;No rtns selected
+ G:'$D(^TMP("XVVVPE","SELECT",$J)) EX ;No rtns selected
  I '$G(FLAGID) D SIGNIN G EX
  D EDIT
 EX ;
  L -^XVV(19200.11)
- KILL ^TMP("VPE","SELECT",$J),^UTILITY($J)
+ KILL ^TMP("XVVVPE","SELECT",$J),^UTILITY($J)
  Q
  ;====================================================================
 GETRTN ;Get rtns to sign in
  I $D(XVSIMERR) S $EC=",U-SIM-ERROR,"
  NEW CNT,DATA,ID,IEN,RTN,TMP,XVVBYI,XVVBYN
  W !,"Building routine list..."
- KILL ^TMP("VPE","SELECT",$J)
- KILL ^TMP("CE",$J)
+ KILL ^TMP("XVVVPE","SELECT",$J)
+ KILL ^TMP("XVVCE",$J)
  S CNT=1
  S RTN=""
  F  S RTN=$O(^XVV(19200.11,"B",RTN)) Q:RTN']""  D  ;
@@ -38,20 +38,20 @@ GETRTN ;Get rtns to sign in
  . I '$G(FLAGALL) Q:XVVBYI'=XVVUSERI
  . S ID=$P(DATA,U,4)
  . S TMP=IEN_$C(9)_RTN_$J("",15-$L(RTN))_"|"_XVVBYN_$J("",30-$L(XVVBYN))_"|"_$E(ID,1,22)
- . S ^TMP("CE",$J,CNT)=TMP
+ . S ^TMP("XVVCE",$J,CNT)=TMP
  . S CNT=CNT+1
  . W "."
  ;
- I '$D(^TMP("CE",$J)) D  Q
+ I '$D(^TMP("XVVCE",$J)) D  Q
  . W !!,"You have no routines signed out."
  . D PAUSE^XVEMKU(2,"P")
  . S FLAGQ=1
  ;
  ;--> Set heading
- S ^TMP("CE",$J,"HD")="ROUTINE         SIGNED OUT BY                  IDENTIFIER"
+ S ^TMP("XVVCE",$J,"HD")="ROUTINE         SIGNED OUT BY                  IDENTIFIER"
  ;--> Run selector
- D SELECT^XVEMKT("^TMP(""CE"","_$J_")",1)
- KILL ^TMP("CE",$J)
+ D SELECT^XVEMKT("^TMP(""XVVCE"","_$J_")",1)
+ KILL ^TMP("XVVCE",$J)
  Q
  ;
 SIGNIN ;Sign in selected rtns by deleting them from the file
@@ -61,7 +61,7 @@ SIGNIN ;Sign in selected rtns by deleting them from the file
  NEW %,%Y,CNT,DA,DATA,DDH,DIC,DIK,DZ,IEN,RTN,X,Y
  S CNT=1
  S IEN=0
- F  S IEN=$O(^TMP("VPE","SELECT",$J,IEN)) Q:IEN'>0  D  ;
+ F  S IEN=$O(^TMP("XVVVPE","SELECT",$J,IEN)) Q:IEN'>0  D  ;
  . S DATA=$G(^(IEN)) Q:DATA']""
  . S RTN=$E($P(DATA,$C(9),2),1,10)
  . S DA=$P(DATA,$C(9),1)
@@ -83,7 +83,7 @@ EDIT ;Bulk edit IDENTIFIER field
  NEW %,%Y,CNT,DA,DATA,DDH,DIC,IEN,RTN,X,Y
  S CNT=1
  S IEN=0
- F  S IEN=$O(^TMP("VPE","SELECT",$J,IEN)) Q:IEN'>0  D  ;
+ F  S IEN=$O(^TMP("XVVVPE","SELECT",$J,IEN)) Q:IEN'>0  D  ;
  . S DATA=$G(^(IEN)) Q:DATA']""
  . S RTN=$E($P(DATA,$C(9),2),1,10)
  . S DA=$P(DATA,$C(9),1)

Routines/XVEMKP.m
@@ -1,13 +1,13 @@
-XVEMKP ;DJB/VRR**Printing [12/30/95 8:23am];2017-08-15  12:58 PM
+XVEMKP ;DJB/VRR**Printing ;2019-08-20  5:31 PM
  ;;15.1;VICTORY PROG ENVIRONMENT;;Jun 19, 2019
  ; Original Code authored by David J. Bolduc 1985-2005
  ;
 TASK(DEFAULT) ;Get device. FLAGQ indicates job has been queued.
  ;DEFAULT=Default Device. Return FLAGQ=1 if taskman is used.
  Q:'$D(^%ZIS)
- S %ZIS="MQ" I $G(DEFAULT)]"" S %ZIS("B")=DEFAULT
+ N %ZIS S %ZIS="MQ" I $G(DEFAULT)]"" S %ZIS("B")=DEFAULT
  W ! D ^%ZIS Q:POP  Q:'$D(IO("Q"))  S FLAGQ=1
- D ^%ZTLOAD,HOME^%ZIS KILL IO("Q")
+ N ZTSK D ^%ZTLOAD,HOME^%ZIS KILL IO("Q")
  I $G(ZTSK)'>0 W !!?1,"Request cancelled.." S FLAGQ=1 Q
  W !!?1,"This task has been queued...Task #",ZTSK,!
  Q

Routines/XVEMKTF.m
@@ -1,11 +1,11 @@
-XVEMKTF ;DJB/KRN**Txt Scroll-Select FM Fields [2/1/97 10:23am];2017-08-15  1:12 PM
+XVEMKTF ;DJB/KRN**Txt Scroll-Select FM Fields ;2019-08-20  5:32 PM
  ;;15.1;VICTORY PROG ENVIRONMENT;;Jun 19, 2019
  ; Original Code authored by David J. Bolduc 1985-2005
  Q
 SELECT(XVVFILE,XVVLEVEL) ;Use Selector to get fields from a FM file.
  ;XVVFILE=File DD number
  ;XVVLEVEL: TOP=Top level flds only  ALL=Include multiple fields
- ;Return array: ^TMP("VPE","FIELDS",$J,File#,Field#)
+ ;Return array: ^TMP("XVV1","FIELDS",$J,File#,Field#)
  ;
  Q:'$G(XVVFILE)
  I '$D(^DIC) D  Q
@@ -21,7 +21,7 @@ SELECT(XVVFILE,XVVLEVEL) ;Use Selector to get fields from a FM file.
  S ^TMP("XVV",$J,"HD")="   "_FILENAM_" file"
  D SELECT^XVEMKT("^TMP(""XVV"","_$J_")")
  D CONVERT
- KILL ^TMP("VPE","SELECT",$J)
+ KILL ^TMP("XVV1","SELECT",$J)
  Q
 BUILD ;Loop and get fields
  NEW BAR,CNT,DASHES,DATA,FILE,FLD,I,LEV,NP,PIECE,SPACE,SYM,TMP
@@ -73,9 +73,9 @@ CONVERT ;Sort Selector array into "File#,Field#" order
  NEW DATA,FIELD,FILE,X
  KILL ^TMP("XVV",$J)
  S X=0
- F  S X=$O(^TMP("VPE","SELECT",$J,X)) Q:'X  D  ;
+ F  S X=$O(^TMP("XVV1","SELECT",$J,X)) Q:'X  D  ;
  . S DATA=$G(^(X)) Q:DATA']""
  . S DATA=$P(DATA,$C(9),1)
  . S FILE=$P(DATA,";",1),FIELD=$P(DATA,";",2)
- . S ^TMP("VPE","FIELDS",$J,FILE,FIELD)=""
+ . S ^TMP("XVV1","FIELDS",$J,FILE,FIELD)=""
  Q

Routines/XVEMKTS.m
@@ -1,4 +1,4 @@
-XVEMKTS ;DJB/KRN**Txt Scroll-SELECTOR ;2017-08-15  1:15 PM
+XVEMKTS ;DJB/KRN**Txt Scroll-SELECTOR ;Aug 20, 2019@17:06
  ;;15.1;VICTORY PROG ENVIRONMENT;;Jun 19, 2019
  ; Original Code authored by David J. Bolduc 1985-2005
  ; New Error trap in TOP+1,INIT+3 (c) 2016 Sam Habiel
@@ -8,7 +8,7 @@ TOP ;
  NEW DX,DY,FLAGQ,XVVT NEW:'$D(XVV) XVV NEW:'$D(XVVS) XVVS
  S FLAGQ=0
  KILL ^TMP("XVV","K",$J)
- KILL ^TMP("VPE","SELECT",$J)
+ KILL ^TMP("XVV1","SELECT",$J)
  D INIT G:FLAGQ EX
  D TEMPLATE
  D SCROLL^XVEMKT2()
@@ -23,7 +23,7 @@ LIST ;Display text
  S XVVT=$G(^TMP("XVV","K",$J,XVVT("BOT")))
  W ! I XVVT=" <> <> <>" W ?3,XVVT
  E  D  ;
- . W:$D(^TMP("VPE","SELECT",$J,XVVT("HLN"))) "=>"
+ . W:$D(^TMP("XVV1","SELECT",$J,XVVT("HLN"))) "=>"
  . W ?3 W:$G(NUMBER) $J(XVVT("HLN"),3)_". "
  . W $S(XVVT[$C(9):$P(XVVT,$C(9),2,999),1:XVVT)
  S XVVT("BOT")=XVVT("BOT")+1
@@ -34,7 +34,7 @@ LIST ;Display text
  G LIST
  ;====================================================================
 TAG ;Tag/Untag a line
- I $D(^TMP("VPE","SELECT",$J,XVVT("HLN")-1)) KILL ^(XVVT("HLN")-1) Q
+ I $D(^TMP("XVV1","SELECT",$J,XVVT("HLN")-1)) KILL ^(XVVT("HLN")-1) Q
  D SET(XVVT("HLN")-1)
  Q
 ALL ;Tag all lines
@@ -53,7 +53,7 @@ CURSORDN ;Tag Cursor-to-Bottom
  D REDRAW^XVEMKT2()
  Q
 CLEAR ;Clear all tagged lines
- KILL ^TMP("VPE","SELECT",$J) D REDRAW^XVEMKT2()
+ KILL ^TMP("XVV1","SELECT",$J) D REDRAW^XVEMKT2()
  Q
 PAGE ;Tag a page
  NEW I
@@ -86,7 +86,7 @@ FIND6 F I=1:1 Q:'$D(^TMP("XVV","K",$J,I))  S DATA=^(I) D  ;
  . I AND="A",$P(DATA,"|",COL1)'[CHAR1!($P(DATA,"|",COL2)'[CHAR2) Q
  . I AND="O",$P(DATA,"|",COL1)'[CHAR1&($P(DATA,"|",COL2)'[CHAR2) Q
  . I MODE="+" D SET(I) Q
- . KILL ^TMP("VPE","SELECT",$J,I) ;Clear tag
+ . KILL ^TMP("XVV1","SELECT",$J,I) ;Clear tag
  D REDRAW^XVEMKT2()
  Q
  ;
@@ -109,7 +109,7 @@ FINDCHR1 W !,"Enter CHARACTERS: "
  ;
 SET(VAL) ;Set "tagged" array. VAL=Line number
  Q:'$D(^TMP("XVV","K",$J,VAL))  Q:^(VAL)=" <> <> <>"
- S ^TMP("VPE","SELECT",$J,VAL)=^TMP("XVV","K",$J,VAL)
+ S ^TMP("XVV1","SELECT",$J,VAL)=^TMP("XVV","K",$J,VAL)
  Q
  ;====================================================================
 TEMPLATE ;Set pre-selected nodes
@@ -118,7 +118,7 @@ TEMPLATE ;Set pre-selected nodes
  S SUB=0
  F  S SUB=$O(@TEMPLATE@(SUB)) Q:'SUB  D  ;
  . Q:'$D(^TMP("XVV","K",$J,SUB))
- . S ^TMP("VPE","SELECT",$J,SUB)=^TMP("XVV","K",$J,SUB)
+ . S ^TMP("XVV1","SELECT",$J,SUB)=^TMP("XVV","K",$J,SUB)
  Q
 INIT ;
  NEW HD,TOT

Routines/XVEMKTT.m
@@ -1,4 +1,4 @@
-XVEMKTT ;DJB/KRN**Txt Scroll-SELECTOR READ ;2017-08-15  1:16 PM
+XVEMKTT ;DJB/KRN**Txt Scroll-SELECTOR READ ;Aug 20, 2019@17:08
  ;;15.1;VICTORY PROG ENVIRONMENT;;Jun 19, 2019
  ; Original Code authored by David J. Bolduc 1985-2005
  ;
@@ -34,9 +34,9 @@ READ1 D HIGHLITE("ON")
  I KEY="-" D FIND^XVEMKTS("-") Q
  I KEY="M" D MORE^XVEMKTU,REDRAW^XVEMKT2() Q
  I KEY="N",$G(NEW)=1 D  S FLAGQ=1 Q
- . KILL ^TMP("VPE","SELECT",$J)
+ . KILL ^TMP("XVV1","SELECT",$J)
  . R KEY:1 ;Pause in case user hits N,<RETURN>
- . S ^TMP("VPE","SELECT",$J,"NEW")=""
+ . S ^TMP("XVV1","SELECT",$J,"NEW")=""
  I KEY="P" D PAGE^XVEMKTS Q
  I KEY="S" D SHOW^XVEMKTU,REDRAW^XVEMKT2() Q
  I VK="<F4T>" D CURSORUP^XVEMKTS Q  ;Tag cursor to top line
@@ -65,7 +65,7 @@ HIGHLITE(MODE) ; MODE="ON"  - Draw highlight
  NEW HL I $G(MODE)'="ON" S MODE="OFF"
  S DX=0,DY=XVVT("H$Y")-1 X XVVS("CRSR")
  W:MODE="ON" @XVV("RON")
- W $S($D(^TMP("VPE","SELECT",$J,XVVT("HLN")-1)):"=>",1:"  ")
+ W $S($D(^TMP("XVV1","SELECT",$J,XVVT("HLN")-1)):"=>",1:"  ")
  W:MODE="ON" @XVV("ROFF")
  Q
  ;====================================================================

Routines/XVEMKTU.m
@@ -1,4 +1,4 @@
-XVEMKTU ;DJB/KRN**Txt Scroll-SELECTOR Help [8/16/97 11:10am];2017-08-15  1:17 PM
+XVEMKTU ;DJB/KRN**Txt Scroll-SELECTOR Help ;2019-08-20  5:33 PM
  ;;15.1;VICTORY PROG ENVIRONMENT;;Jun 19, 2019
  ; Original Code authored by David J. Bolduc 1985-2005
  ;
@@ -40,9 +40,9 @@ SHOW ;Show selected items
  NEW FLAGQ,I,ITEM
  W !,"S E L E C T E D   I T E M S"
  S (FLAGQ,ITEM)=0
- F  S ITEM=$O(^TMP("VPE","SELECT",$J,ITEM)) Q:'ITEM!FLAGQ  D  ;
- . W !,$P($G(^TMP("VPE","SELECT",$J,ITEM)),$C(9),2)
- . I '$O(^TMP("VPE","SELECT",$J,ITEM)) D  S FLAGQ=1 Q
+ F  S ITEM=$O(^TMP("XVV1","SELECT",$J,ITEM)) Q:'ITEM!FLAGQ  D  ;
+ . W !,$P($G(^TMP("XVV1","SELECT",$J,ITEM)),$C(9),2)
+ . I '$O(^TMP("XVV1","SELECT",$J,ITEM)) D  S FLAGQ=1 Q
  . . F I=$Y:1:(XVV("IOSL")-4) W !
  . . D PAUSE^XVEMKC(1)
  . I $Y>(XVV("IOSL")-4) D PAUSEQ^XVEMKC(1) Q:FLAGQ  W @XVV("IOF")

Routines/XVEMRID.m
@@ -1,16 +1,16 @@
-XVEMRID ;DJB/VRR**INSERT - Programmer Call ;2017-08-15  1:56 PM
+XVEMRID ;DJB/VRR**INSERT - Programmer Call ;Aug 20, 2019@17:11
  ;;15.1;VICTORY PROG ENVIRONMENT;;Jun 19, 2019
  ; Original Code authored by David J. Bolduc 1985-2005
  ; New Error trap in EN (c) 2016 Sam Habiel
  ;
 EN ;Insert programmer call into current program
- S ^TMP("VPE",$J)=$P(FLAGMENU,"^",1) ;...YND
+ S ^TMP("XVV1",$J)=$P(FLAGMENU,"^",1) ;...YND
  D SYMTAB^XVEMKST("C","VRR",VRRS) ;...Save/Clear symbol table
  D ENDSCR^XVEMKT2
  I '$D(^XVV(19200.113)) D  G EX ;...Quit if FM isn't in this UCI
  . W !,"I can't find the VPE files that support CALL..."
  . D PAUSE^XVEMKU(2,"P")
- I $G(^TMP("XVV","IR"_VRRS,$J,^TMP("VPE",$J)))=" <> <> <>" D  G EX
+ I $G(^TMP("XVV","IR"_VRRS,$J,^TMP("XVV1",$J)))=" <> <> <>" D  G EX
  . W !,"You may not insert a CALL while you are on the ' <> <> <>' line."
  . W !,"Move up one line."
  . D PAUSE^XVEMKU(2,"P")
@@ -27,7 +27,7 @@ EN ;Insert programmer call into current program
  G:$$ASK^XVEMKU(" Insert this Call into your routine",1)'="Y" EX
  D INSERT
 EX ;
- KILL ^TMP("VPE",$J)
+ KILL ^TMP("XVV1",$J)
  D SYMTAB^XVEMKST("R","VRR",VRRS) ;Restore symbol table
  X XVVS("RM0")
  Q
@@ -51,7 +51,7 @@ DDS ;Call ScreenMan
  Q
 INSERT ;Insert Call code into rtn
  NEW CNT,I,NUM,SUB,YND
- S YND=$G(^TMP("VPE",$J)) Q:YND'>0
+ S YND=$G(^TMP("XVV1",$J)) Q:YND'>0
  ;--> Get line number
  S NUM=$$LINENUM^XVEMRU(YND)+1
  ;--> Set YND to line number after wrapped lines

Routines/XVEMRLZ.m
@@ -1,4 +1,4 @@
-XVEMRLZ ;DJB/VRR**RTN VER - ..LBRY Options ;2017-08-15  2:08 PM
+XVEMRLZ ;DJB/VRR**RTN VER - ..LBRY Options ;Aug 20, 2019@17:14
  ;;15.1;VICTORY PROG ENVIRONMENT;;Jun 19, 2019
  ; Original Code authored by David J. Bolduc 1985-2005
  ; New Error traps in DELETE and BULKDEL (c) 2016 Sam Habiel
@@ -17,7 +17,7 @@ DELETE ;Delete versions
  . W $C(7),!!,"This program is currently being edited. Try later.",!
  . D PAUSE^XVEMKU(2,"P")
  ;
- KILL ^TMP("VPE",$J)
+ KILL ^TMP("XVV1",$J)
  S CNT=1,VER=0
  F  S VER=$O(^XVV(19200.112,"AKEY",RTN,VER)) Q:'VER  D  ;
  . I $D(XVSIMERR) S $EC=",U-SIM-ERROR,"
@@ -26,21 +26,21 @@ DELETE ;Delete versions
  . S DESC=$P(ND,"^",3) S:DESC="" DESC="No description"
  . S TMP="Version: "_VER
  . S TMP=TMP_$J("",15-$L(TMP))_"|"_DESC
- . S ^TMP("VPE",$J,CNT)=IEN_$C(9)_TMP
+ . S ^TMP("XVV1",$J,CNT)=IEN_$C(9)_TMP
  . S CNT=CNT+1
  ;
- G:'$D(^TMP("VPE",$J)) DELEX ;.............No versions on file
- S ^TMP("VPE",$J,"HD")="^"_RTN_" routine "
- D SELECT^XVEMKT("^TMP(""VPE"","_$J_")") ;Call SELECTOR
- G:'$D(^TMP("VPE","SELECT",$J)) DELEX  ;...Nothing selected
+ G:'$D(^TMP("XVV1",$J)) DELEX ;.............No versions on file
+ S ^TMP("XVV1",$J,"HD")="^"_RTN_" routine "
+ D SELECT^XVEMKT("^TMP(""XVV1"","_$J_")") ;Call SELECTOR
+ G:'$D(^TMP("XVV1","SELECT",$J)) DELEX  ;...Nothing selected
  ;
  S RENUM=$$ASK^XVEMKU("Renumber any remaining versions, starting with 1",1)
  W !
  G:$$ASK^XVEMKU("Ok to delete now",1)'="Y" DELEX
  ;
  S CNT=0
- F  S CNT=$O(^TMP("VPE","SELECT",$J,CNT)) Q:'CNT  D  ;
- . S ND=$G(^TMP("VPE","SELECT",$J,CNT))
+ F  S CNT=$O(^TMP("XVV1","SELECT",$J,CNT)) Q:'CNT  D  ;
+ . S ND=$G(^TMP("XVV1","SELECT",$J,CNT))
  . S DA=$P(ND,$C(9),1) Q:'DA
  . S DIK="^XVV(19200.112,"
  . D ^DIK
@@ -51,8 +51,8 @@ DELETE ;Delete versions
  D PAUSE^XVEMKU(2)
 DELEX ;
  L -VRRLOCK(RTN) ;Unlock routine editing
- KILL ^TMP("VPE",$J)
- KILL ^TMP("VPE","SELECT",$J)
+ KILL ^TMP("XVV1",$J)
+ KILL ^TMP("XVV1","SELECT",$J)
  Q
  ;
 DELRENUM(RTN) ;Renumber remaining versions.
@@ -75,16 +75,16 @@ BULKDEL ;Bulk delete
  ;
  Q:'$D(^XVV(19200.112))  ;...Version file doesn't exist
  N $ES,$ET S $ET="D ERROR^XVEMRLZ,UNWIND^XVEMSY"
- KILL ^TMP("VPE",$J)
+ KILL ^TMP("XVV1",$J)
  ;
  S SHOW=$$BULKS() G:SHOW="" BULKEX
  I SHOW="L" D BULKL I 1 ;Show only Library routines in Selector
  E  D BULKA ;Show ALL routines in Selector
  ;
- G:'$D(^TMP("VPE",$J)) BULKEX ;............No versions on file
- S ^TMP("VPE",$J,"HD")="Routines on file "
- D SELECT^XVEMKT("^TMP(""VPE"","_$J_")") ;Call SELECTOR
- G:'$D(^TMP("VPE","SELECT",$J)) BULKEX  ;..Nothing selected
+ G:'$D(^TMP("XVV1",$J)) BULKEX ;............No versions on file
+ S ^TMP("XVV1",$J,"HD")="Routines on file "
+ D SELECT^XVEMKT("^TMP(""XVV1"","_$J_")") ;Call SELECTOR
+ G:'$D(^TMP("XVV1","SELECT",$J)) BULKEX  ;..Nothing selected
  ;
  S PRESERVE=$$BULKV() ;Preserve any versions?
  I PRESERVE W ! D  ;
@@ -96,8 +96,8 @@ BULKDEL ;Bulk delete
  D PAUSE^XVEMKU(2)
  ;
 BULKEX ;Exit
- KILL ^TMP("VPE",$J)
- KILL ^TMP("VPE","SELECT",$J)
+ KILL ^TMP("XVV1",$J)
+ KILL ^TMP("XVV1","SELECT",$J)
  Q
  ;
 BULKS() ;Which routines to show in Selector?
@@ -125,7 +125,7 @@ BULKA ;Present all routines in Selector
  . S (NUM,VER)=0
  . F  S VER=$O(^XVV(19200.112,"AKEY",RTN,VER)) Q:'VER  S NUM=NUM+1
  . S TMP=TMP_"|"_NUM_" version"_$S(NUM>1:"s",1:"")
- . S ^TMP("VPE",$J,CNT)=RTN_"^"_NUM_$C(9)_TMP
+ . S ^TMP("XVV1",$J,CNT)=RTN_"^"_NUM_$C(9)_TMP
  . S CNT=CNT+1
  Q
  ;
@@ -145,15 +145,15 @@ BULKL ;Present only those routines signed out in the Library
  . S (NUM,VER)=0
  . F  S VER=$O(^XVV(19200.112,"AKEY",RTN,VER)) Q:'VER  S NUM=NUM+1
  . S TMP=TMP_"|"_NUM_" version"_$S(NUM>1:"s",1:"")
- . S ^TMP("VPE",$J,CNT)=RTN_"^"_NUM_$C(9)_TMP
+ . S ^TMP("XVV1",$J,CNT)=RTN_"^"_NUM_$C(9)_TMP
  . S CNT=CNT+1
  Q
  ;
 BULKD ;Do deletions
  NEW CNT,DA,DIC,DIK,I,ND,NUM,TMP,RTN,VER
  S CNT=0
- F  S CNT=$O(^TMP("VPE","SELECT",$J,CNT)) Q:'CNT  D  ;
- . S ND=$G(^TMP("VPE","SELECT",$J,CNT))
+ F  S CNT=$O(^TMP("XVV1","SELECT",$J,CNT)) Q:'CNT  D  ;
+ . S ND=$G(^TMP("XVV1","SELECT",$J,CNT))
  . S TMP=$P(ND,$C(9),1)
  . S RTN=$P(TMP,"^",1) Q:RTN']""  ;Routine name
  . S NUM=$P(TMP,"^",2) ;Number of versions on file

commit e4481533874155959dcb30d7485dc427b83d0115

Fix bug introduced in 5779b4a5bd4d91d0c40b9446a5da921d1ac2e20c
^TMP namespace needs to be the same in the selector, otherwise, we can't
find the data.

Routines/XVEMRLI.m
@@ -1,4 +1,4 @@
-XVEMRLI ;DJB/VRR**RTN LBRY - Sign In Rtns,ALL,Edit IDENTIFIER ;Aug 20, 2019@16:51
+XVEMRLI ;DJB/VRR**RTN LBRY - Sign In Rtns,ALL,Edit IDENTIFIER ;Aug 26, 2019@14:08
  ;;15.1;VICTORY PROG ENVIRONMENT;;Jun 19, 2019
  ; Original Code authored by David J. Bolduc 1985-2005
  ; New Error trap in EN+2,GETRTN (c) 2016 Sam Habiel
@@ -13,19 +13,19 @@ EN ;Use ALL^XVEMRLI if you need to sign in rtns other than your own.
  Q:'$D(^XVV(19200.11))  ;...Library file doesn't exist
  S FLAGQ=0 D INIT^XVEMRLU G:FLAGQ EX
  D GETRTN G:FLAGQ EX
- G:'$D(^TMP("XVVVPE","SELECT",$J)) EX ;No rtns selected
+ G:'$D(^TMP("XVV1","SELECT",$J)) EX ;No rtns selected
  I '$G(FLAGID) D SIGNIN G EX
  D EDIT
 EX ;
  L -^XVV(19200.11)
- KILL ^TMP("XVVVPE","SELECT",$J),^UTILITY($J)
+ KILL ^TMP("XVV1","SELECT",$J),^UTILITY($J)
  Q
  ;====================================================================
 GETRTN ;Get rtns to sign in
  I $D(XVSIMERR) S $EC=",U-SIM-ERROR,"
  NEW CNT,DATA,ID,IEN,RTN,TMP,XVVBYI,XVVBYN
  W !,"Building routine list..."
- KILL ^TMP("XVVVPE","SELECT",$J)
+ KILL ^TMP("XVV1","SELECT",$J)
  KILL ^TMP("XVVCE",$J)
  S CNT=1
  S RTN=""
@@ -61,7 +61,7 @@ SIGNIN ;Sign in selected rtns by deleting them from the file
  NEW %,%Y,CNT,DA,DATA,DDH,DIC,DIK,DZ,IEN,RTN,X,Y
  S CNT=1
  S IEN=0
- F  S IEN=$O(^TMP("XVVVPE","SELECT",$J,IEN)) Q:IEN'>0  D  ;
+ F  S IEN=$O(^TMP("XVV1","SELECT",$J,IEN)) Q:IEN'>0  D  ;
  . S DATA=$G(^(IEN)) Q:DATA']""
  . S RTN=$E($P(DATA,$C(9),2),1,10)
  . S DA=$P(DATA,$C(9),1)
@@ -83,7 +83,7 @@ EDIT ;Bulk edit IDENTIFIER field
  NEW %,%Y,CNT,DA,DATA,DDH,DIC,IEN,RTN,X,Y
  S CNT=1
  S IEN=0
- F  S IEN=$O(^TMP("XVVVPE","SELECT",$J,IEN)) Q:IEN'>0  D  ;
+ F  S IEN=$O(^TMP("XVV1","SELECT",$J,IEN)) Q:IEN'>0  D  ;
  . S DATA=$G(^(IEN)) Q:DATA']""
  . S RTN=$E($P(DATA,$C(9),2),1,10)
  . S DA=$P(DATA,$C(9),1)